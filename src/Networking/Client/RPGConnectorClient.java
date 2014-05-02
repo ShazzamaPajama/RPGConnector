8b33b2c9fb2a0c9dd0a43221da4694847ba1e4cd
Paul Koroski <Shazzama.Pajama@gmail.com>
2014-05-02 10:23:28 -0400

    Creates a BufferedReader to read messages from the server

--- src/Networking/Client/RPGConnectorClient.java
+++ src/Networking/Client/RPGConnectorClient.java
@@ -6,6 +6,9 @@
 
 package Networking.Client;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
@@ -15,10 +18,20 @@ import java.net.Socket;
  */
 public class RPGConnectorClient {
     private Socket Client;
-    private PrintWriter ClientOuputStream;
+    private PrintWriter ServerWriter;
     private ClientMessageBuilder MessageBuilder;
-    private ServerMessagesThread ProcessingThread;
+    private ClientThread ProcessingThread;
+    private BufferedReader ServerReader;
     
     
+    public RPGConnectorClient(String Host) throws IOException{
+        Client = new Socket(Host, 1337);
+        ServerWriter = new PrintWriter(Client.getOutputStream(), true);
+        MessageBuilder = new ClientMessageBuilder();
+        ServerReader = new BufferedReader(new InputStreamReader(Client.getInputStream()));
+    }
     
+    public void StartClientThread(){
+        new ClientThread(ServerReader).start();
+    }
 }
