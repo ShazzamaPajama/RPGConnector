6ca29c44c8f84a9624a052f6e87ab405cc30c291
Paul Koroski <Shazzama.Pajama@gmail.com>
2014-05-02 10:21:51 -0400

    Creates Printwriter for client and adds to the server

--- src/Networking/Server/UserThread.java
+++ src/Networking/Server/UserThread.java
@@ -9,6 +9,7 @@ package Networking.Server;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.io.StringReader;
 import java.net.Socket;
 import java.util.logging.Level;
@@ -33,6 +34,9 @@ public class UserThread extends Thread {
         MessageReader = new UserMessageProcessor(Server);
         Client = client;
         ClientReader = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        
+        PrintWriter ClientWriter = new PrintWriter(Client.getOutputStream(), true);
+        Server.addClientOutput(ClientWriter);
     }
     
     
